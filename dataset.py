1f65634cb20c8fb0ccc13b827c79a8c3187a74ca
Vince <vincivenv@gmail.com>
2022-08-28 23:38:23 +0200

    datamodule, ds

---
+++ dataset.py
@@ -0,0 +1,27 @@
+import os
+
+from PIL import Image
+from torch.utils.data import Dataset
+
+
+class AmazonDataset(Dataset):
+    def __init__(self, df, ohe_tags, transform, path):
+        super().__init__()
+        self.df = df
+        self.ohe_tags = ohe_tags
+        self.transform = transform
+        self.path = path
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        filename = self.df.iloc[idx].image_name + ".jpg"
+        if filename in os.listdir(self.path):
+            file_path = os.path.join(self.path, filename)
+        else:
+            raise Exception(f"Can't fetch {filename} among {self.paths}")
+        img = Image.open(file_path).convert("RGB")
+        label = self.ohe_tags[idx]
+        img = self.transform(img)
+        return img, label
---
+++ train.py
@@ -0,0 +1,130 @@
+from pathlib import Path
+
+import fire
+import pandas as pd
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks.progress import TQDMProgressBar
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MultiLabelBinarizer
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+from torchvision import models, transforms
+
+from dataset import AmazonDataset
+
+
+class Model(pl.LightningModule):
+    def __init__(self, classes=17):
+        super().__init__()
+        self.save_hyperparameters()
+
+        model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+        for param in model.parameters():
+            param.require_grad = False
+        model.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        model.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(512, 128),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.2),
+            nn.Linear(128, classes),
+            nn.Sigmoid(),
+        )
+        self.model = model
+
+    def forward(self, x):
+        return torch.relu(self.model(x.view(x.size(0), -1)))
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        tensorboard_logs = {"train_loss": loss}
+        return {"loss": loss, "log": tensorboard_logs}
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        return {"val_loss": loss}
+
+    def validation_epoch_end(self, outputs):
+        avg_loss = torch.stack([x["val_loss"] for x in outputs]).mean()
+        return {"val_loss": avg_loss}
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.001)
+
+
+class Datamodule:
+    def __init__(self, data_dir, bs=64):
+        super().__init__()
+        self.trsfms = transforms.Compose(
+            [
+                transforms.Resize(224),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406],
+                    std=[0.229, 0.224, 0.225],
+                ),
+            ]
+        )
+        self.data_dir = data_dir
+        self.bs = bs
+        df = pd.read_csv(Path(self.data_dir) / "train_v2.csv")
+        df["list_tags"] = df.tags.str.split(" ")
+        encoder = MultiLabelBinarizer()
+        train_data, val_data = train_test_split(df, test_size=0.2)
+        ohe_train_tags = encoder.fit_transform(train_data.list_tags.values)
+        ohe_val_tags = encoder.fit_transform(val_data.list_tags.values)
+        self.train_dataset = AmazonDataset(
+            train_data,
+            ohe_tags=ohe_train_tags,
+            transform=self.trsfms,
+            path=Path(self.data_dir) / "train",
+        )
+        self.val_dataset = AmazonDataset(
+            val_data,
+            ohe_tags=ohe_val_tags,
+            transform=self.trsfms,
+            path=Path(self.data_dir) / "train",
+        )
+
+
+def runner(path):
+    model = Model()
+    data = Datamodule(data_dir=path)
+    train_loader = DataLoader(
+        data.train_dataset,
+        batch_size=64,
+        shuffle=True,
+    )
+    val_dataloader = DataLoader(
+        data.val_dataset,
+        batch_size=64,
+        shuffle=True,
+    )
+
+    checkpoint_callback = ModelCheckpoint(
+        monitor="val_loss",
+        dirpath="ckpt",
+        filename="model-epoch{epoch:02d}-val_loss{val/loss:.2f}",
+        auto_insert_metric_name=True,
+    )
+    trainer = pl.Trainer(
+        callbacks=[checkpoint_callback, TQDMProgressBar(refresh_rate=20)],
+        accelerator="gpu",
+        enable_progress_bar=True,
+        enable_model_summary=True,
+        max_epochs=10,
+        fast_dev_run=True,
+        enable_checkpointing=True,
+    )
+    trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_dataloader)
+
+
+if __name__ == "__main__":
+    fire.Fire(runner)
